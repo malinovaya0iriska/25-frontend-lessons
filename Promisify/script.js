8b3ccc28bd57f3b57e6c1daac55c7f12ea3bb064
Irina Martsulevich <malinovayairiska@gmail.com>
2022-06-07 15:16:22 +0300

    feat: add promisification fn

---
+++ Promisify/script.js
@@ -0,0 +1,18 @@
+// function foo (...args, otherFn) {};
+
+// function otherFn(error, value) {};
+
+function promisify(callback) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function handleErrorAndValue(error, value) {
+        if (error === null) {
+          resolve(value);
+        } else {
+          reject(error);
+        }
+      }
+      callback.call(this, ...args, handleErrorAndValue);
+    });
+  };
+}
