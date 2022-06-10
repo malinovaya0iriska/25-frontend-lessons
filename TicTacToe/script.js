a937dad56f4b89390800334e1594c766c04c6e3b
Irina Martsulevich <malinovayairiska@gmail.com>
2022-06-10 17:08:35 +0300

    feat: another variant of tic tac game

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,5 +1,16 @@
 const BOARD_WIDTH = 3;
 
+const WIN_CONDITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 let boardState = generateEmptyBoardState();
 let currentPlayer = 1;
 let numMovesDone = 0;
@@ -10,21 +21,18 @@ const restartButton = document.getElementById('restart-button');
 
 gameSquares.forEach((gameSquare, i) => {
   gameSquare.addEventListener('click', () => {
-    const row = Math.floor(i / BOARD_WIDTH);
-    const col = i % BOARD_WIDTH;
-    makeMove(gameSquare, row, col);
+    makeMove(gameSquare);
   });
 });
 
 restartButton.addEventListener('click', restartGame);
 
-function makeMove(gameSquare, row, col) {
+function makeMove(gameSquare) {
   gameSquare.textContent = currentPlayer === 1 ? 'X' : 'O';
   gameSquare.disabled = true;
   numMovesDone++;
-  boardState[row][col] = currentPlayer;
 
-  if (didPlayerWin(currentPlayer)) {
+  if (didPlayerWin()) {
     gameHeading.textContent = `Player ${currentPlayer} Won!`;
     endGame();
   } else if (numMovesDone >= BOARD_WIDTH * BOARD_WIDTH) {
@@ -40,42 +48,13 @@ function setCurrentPlayerHeader() {
   gameHeading.textContent = `Player ${currentPlayer}'s Turn`;
 }
 
-function didPlayerWin(player) {
-  const rows = [0, 1, 2];
-  const cols = [0, 1, 2];
-
-  const wonHorizontally = rows.some((row) => {
-    return (
-      boardState[row][0] === currentPlayer &&
-      boardState[row][1] === currentPlayer &&
-      boardState[row][2] === currentPlayer
-    );
+function didPlayerWin() {
+  const relevantText = currentPlayer === 1 ? 'X' : 'O';
+  return WIN_CONDITIONS.some((condition) => {
+    return condition.every((gameSquarePosition) => {
+      return gameSquares[gameSquarePosition].textContent === relevantText;
+    });
   });
-
-  const wonVertically = cols.some((col) => {
-    return (
-      boardState[0][col] === currentPlayer &&
-      boardState[1][col] === currentPlayer &&
-      boardState[2][col] === currentPlayer
-    );
-  });
-
-  const wonTopLeftToBottomRight =
-    boardState[0][0] === currentPlayer &&
-    boardState[1][1] === currentPlayer &&
-    boardState[2][2] === currentPlayer;
-
-  const wonTopRightToBottomLeft =
-    boardState[2][0] === currentPlayer &&
-    boardState[1][1] === currentPlayer &&
-    boardState[0][2] === currentPlayer;
-
-  return (
-    wonHorizontally ||
-    wonVertically ||
-    wonTopLeftToBottomRight ||
-    wonTopRightToBottomLeft
-  );
 }
 
 function endGame() {
